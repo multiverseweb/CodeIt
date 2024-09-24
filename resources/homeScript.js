8ce2cef3e7008d475897fe17a0ed995ddb3372d4
Tejas Gupta <multiverse.letter@gmail.com>
2024-09-25 04:34:40 +0530

    Update homeScript.js

--- resources/homeScript.js
+++ resources/homeScript.js
@@ -1,5 +1,5 @@
 //list of cities
-var cities = ["Pune", "Moradabad", "Dehradun","Ujjain","Vijaywada","Rampur"];
+var cities = ["Pune", "Moradabad", "Dehradun","Ujjain","Vijaywada","Rampur","Sydney"];
 
 function topFunction() {
     document.body.scrollTop = 0;
