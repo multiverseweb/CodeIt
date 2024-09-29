b0ee8363ee32aebccc9dbba5e7e590eba387f2e5
Wasid786 <114327679+Wasid786@users.noreply.github.com>
2024-09-29 23:17:37 +0530

    align highlight div with nav_table

--- resources/common_functions.js
+++ resources/common_functions.js
@@ -1,120 +1,126 @@
 console.log("Tejas' Codes :D");
 
-const highlight = document.querySelector('.highlight');
-const navItems = document.querySelectorAll('.nav-item');
+const highlight = document.querySelector(".highlight");
+const navItems = document.querySelectorAll(".nav-item");
 
-navItems.forEach(item => {
-    item.addEventListener('mouseover', function () {
-        const itemRect = item.getBoundingClientRect();
-        const tableRect = item.closest('table').getBoundingClientRect(); // Get the table's bounding box
+navItems.forEach((item) => {
+  item.addEventListener("mouseover", function () {
+    const itemRect = item.getBoundingClientRect();
+    const tableRect = item.closest("table").getBoundingClientRect(); // Get the table's bounding box
 
-        // Adjust the highlight position and width based on the table's position
-        highlight.style.width = `${itemRect.width}px`;
-        highlight.style.left = `${itemRect.left - tableRect.left}px`;
-        highlight.style.top = `${itemRect.top - tableRect.top}px`;
-    });
+    // Adjust the highlight position and width based on the table's position
+    highlight.style.width = `${itemRect.width}px`;
+    highlight.style.left = `${itemRect.left}px`;
+    highlight.style.top = `${itemRect.top - tableRect.top}px`;
+  });
 });
 
-document.querySelector('.navbar').addEventListener('mouseleave', () => {
-    highlight.style.width = '0';
+document.querySelector(".navbar").addEventListener("mouseleave", () => {
+  highlight.style.width = "0";
 });
-
 
 function light() {
-    document.getElementById("themeLogo").src = "../images/sun.png";
-    document.getElementById("themeLogo").style.filter = "invert(0)";
-    const input_code = document.getElementById("input_code");
-    if (input_code) {
-        input_code.style.color = "#0b6100";
-        document.getElementById("output_code").style.color = "#002d8f";
-        document.getElementById("submit").style.filter = "invert(1)";
-    } else {
-        const normal_code = document.getElementById("normal_code");
-        if (normal_code) {
-            normal_code.style.color = "#0b6100";
-            document.getElementById("obfuscated_code").style.color = "#002d8f";
-        }
+  document.getElementById("themeLogo").src = "../images/sun.png";
+  document.getElementById("themeLogo").style.filter = "invert(0)";
+  const input_code = document.getElementById("input_code");
+  if (input_code) {
+    input_code.style.color = "#0b6100";
+    document.getElementById("output_code").style.color = "#002d8f";
+    document.getElementById("submit").style.filter = "invert(1)";
+  } else {
+    const normal_code = document.getElementById("normal_code");
+    if (normal_code) {
+      normal_code.style.color = "#0b6100";
+      document.getElementById("obfuscated_code").style.color = "#002d8f";
     }
-    const time_code = document.getElementById("time_code");
-    if (time_code) {
-        time_code.style.color = "#0b6100";
-    }
-    const heading = document.getElementById("heading");
-    if (heading) {
-        document.getElementById("body").style.color="rgba(0,0,0, 0.866)";
-        document.getElementById("shadow").style.backgroundImage="linear-gradient(115deg, #00000000,rgb(220, 220, 220),#00000000)";
-    }
-    document.getElementById("link").style.filter = "invert(1)";
-    document.getElementById("link2").style.filter = "invert(0)";
-    document.getElementById("body").style.backgroundColor = "rgb(220, 220, 220)";
-    const button = document.getElementById('theme');
-    button.onclick = dark;
+  }
+  const time_code = document.getElementById("time_code");
+  if (time_code) {
+    time_code.style.color = "#0b6100";
+  }
+  const heading = document.getElementById("heading");
+  if (heading) {
+    document.getElementById("body").style.color = "rgba(0,0,0, 0.866)";
+    document.getElementById("shadow").style.backgroundImage =
+      "linear-gradient(115deg, #00000000,rgb(220, 220, 220),#00000000)";
+  }
+  document.getElementById("link").style.filter = "invert(1)";
+  document.getElementById("link2").style.filter = "invert(0)";
+  document.getElementById("body").style.backgroundColor = "rgb(220, 220, 220)";
+  const button = document.getElementById("theme");
+  button.onclick = dark;
 }
 
 function dark() {
-    document.getElementById("themeLogo").src = "../images/moon.png";
-    document.getElementById("themeLogo").style.filter = "invert(1)";
-    const input_code = document.getElementById("input_code");
-    if (input_code) {
-        input_code.style.color = "rgba(255, 170, 0, 0.756)";
-        document.getElementById("output_code").style.color = "rgba(0,0,0,1)";
-        document.getElementById("submit").style.filter = "invert(0)";
-    } else {
-        const normal_code = document.getElementById("normal_code");
-        if (normal_code) {
-            normal_code.style.color = "rgba(255, 170, 0, 0.756)";
-            document.getElementById("obfuscated_code").style.color = "rgba(0, 255, 204, 0.619)";
-        }
+  document.getElementById("themeLogo").src = "../images/moon.png";
+  document.getElementById("themeLogo").style.filter = "invert(1)";
+  const input_code = document.getElementById("input_code");
+  if (input_code) {
+    input_code.style.color = "rgba(255, 170, 0, 0.756)";
+    document.getElementById("output_code").style.color = "rgba(0,0,0,1)";
+    document.getElementById("submit").style.filter = "invert(0)";
+  } else {
+    const normal_code = document.getElementById("normal_code");
+    if (normal_code) {
+      normal_code.style.color = "rgba(255, 170, 0, 0.756)";
+      document.getElementById("obfuscated_code").style.color =
+        "rgba(0, 255, 204, 0.619)";
     }
-    const time_code = document.getElementById("time_code");
-    if (time_code) {
-        time_code.style.color = "rgba(255, 170, 0, 0.756)";
-    }
-    const heading = document.getElementById("heading");
-    if (heading) {
-        document.getElementById("body").style.color="rgba(255, 255, 255, 0.866)";
-        document.getElementById("shadow").style.backgroundImage="linear-gradient(115deg, #00000000,rgba(30,30,30, 0.888),rgba(30,30,30, 0.888),#00000000)";
-    }
-    document.getElementById("link").style.filter = "invert(0)";
-    document.getElementById("link2").style.filter = "invert(1)";
-    document.getElementById("body").style.backgroundColor = "rgba(0, 0, 0, 0.888)";
-    const button = document.getElementById('theme');
-    button.onclick = light;
+  }
+  const time_code = document.getElementById("time_code");
+  if (time_code) {
+    time_code.style.color = "rgba(255, 170, 0, 0.756)";
+  }
+  const heading = document.getElementById("heading");
+  if (heading) {
+    document.getElementById("body").style.color = "rgba(255, 255, 255, 0.866)";
+    document.getElementById("shadow").style.backgroundImage =
+      "linear-gradient(115deg, #00000000,rgba(30,30,30, 0.888),rgba(30,30,30, 0.888),#00000000)";
+  }
+  document.getElementById("link").style.filter = "invert(0)";
+  document.getElementById("link2").style.filter = "invert(1)";
+  document.getElementById("body").style.backgroundColor =
+    "rgba(0, 0, 0, 0.888)";
+  const button = document.getElementById("theme");
+  button.onclick = light;
 }
 function copylink() {
-    navigator.clipboard.writeText("https://codeittool.netlify.app/").then(() => {
-        const linkElement = document.getElementById("link");
+  navigator.clipboard
+    .writeText("https://codeittool.netlify.app/")
+    .then(() => {
+      const linkElement = document.getElementById("link");
 
-        // Temporarily change the content to the checkmark ✓
-        linkElement.innerHTML = "✓&nbsp;&nbsp;";
+      // Temporarily change the content to the checkmark ✓
+      linkElement.innerHTML = "✓&nbsp;&nbsp;";
 
-        // After 2 seconds, change it back to the original image
-        setTimeout(() => {
-            linkElement.innerHTML = `<img src="https://icons.veryicon.com/png/o/miscellaneous/foundation-icon-5/link-86.png" alt="">`;
-        }, 2000);
-    }).catch(err => alert('Copy failed: ', err));
+      // After 2 seconds, change it back to the original image
+      setTimeout(() => {
+        linkElement.innerHTML = `<img src="https://icons.veryicon.com/png/o/miscellaneous/foundation-icon-5/link-86.png" alt="">`;
+      }, 2000);
+    })
+    .catch((err) => alert("Copy failed: ", err));
 }
 
 function copied(elementId) {
-    const element = document.getElementById(elementId);
-    const originalText = element.innerHTML;
-    const originalWidth = element.offsetWidth;
-    element.innerHTML = 'Copied!';
-    element.style.width = `${originalWidth}px`;
-    setTimeout(() => {
-        element.innerHTML = originalText;
-        element.style.width = '';
-    }, 1500);
+  const element = document.getElementById(elementId);
+  const originalText = element.innerHTML;
+  const originalWidth = element.offsetWidth;
+  element.innerHTML = "Copied!";
+  element.style.width = `${originalWidth}px`;
+  setTimeout(() => {
+    element.innerHTML = originalText;
+    element.style.width = "";
+  }, 1500);
 }
 
 function clearText() {
-    const normal_code = document.getElementById("normal_code");
-    if (normal_code) {
-        normal_code.value = "";
-        obfuscated_code.value = "";
-    }
-    const time_code = document.getElementById("time_code");
-    if (time_code) {
-        time_code.value = "";
-    }
+  const normal_code = document.getElementById("normal_code");
+  if (normal_code) {
+    normal_code.value = "";
+    obfuscated_code.value = "";
+  }
+  const time_code = document.getElementById("time_code");
+  if (time_code) {
+    time_code.value = "";
+  }
 }
