ecd4390f0bf61f11942adf473994c56d26f75c9f
Shreya Pandey <shreya02p25@gmail.com>
2024-10-06 19:23:08 +0530

    Add feature license section

--- resources/homeScript.js
+++ resources/homeScript.js
@@ -1,5 +1,5 @@
 //list of cities
-var cities = ["Pune", "Moradabad", "Dehradun","Ujjain","Vijaywada","Rampur","Sydney","Aligarh","Delhi","Coimbatore","Dibrugarh"];
+var cities = ["Pune", "Moradabad", "Dehradun","Ujjain","Vijaywada","Rampur","Sydney","Aligarh","Delhi","Coimbatore","Dibrugarh","Varanasi"];
 
 function topFunction() {
     document.body.scrollTop = 0;
